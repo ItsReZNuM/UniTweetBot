600085dfe3837cc11cb636b720aae68bfea9cc4b
ItsReZNuM <rmohamadnia85@gmail.com>
2025-12-13 21:02:02 +0330

    Fix Saving DB Bug

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ ADMIN_USER_IDS = [int(x.strip()) for x in admin_ids_str.split(',') if x.strip().
 
 ADMIN_ID = ADMIN_USER_IDS[0] if ADMIN_USER_IDS else None
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_NAME = os.path.join(BASE_DIR, os.getenv("DATABASE_NAME", "database.db"))
+
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
 DEFAULT_TWEET_HOURS = [int(x.strip()) for x in os.getenv("DEFAULT_TWEET_HOURS", "9,12,15,18,21,0").split(',') if x.strip().isdigit()]
